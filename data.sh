db6c1b471f13bee4ff7ff06eddd38be35617fca9
thib <thibautfenain1@gmail.com>
2019-12-31 17:57:34 +0800

    first

---
+++ data.sh
@@ -0,0 +1,8 @@
+# Download dataset from Dropbox
+wget https://www.dropbox.com/s/1xdhakipujfzx7l/dataset.zip?dl=0
+
+# Unzip the downloaded zip file
+unzip ./dataset.zip
+
+# Remove the downloaded zip file
+rm ./dataset.zip
